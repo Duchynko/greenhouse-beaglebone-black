6c1276b9d4facdde6af5dbea6596354eb9606f73
Jakub Duchon <jakub.duchon9@gmail.com>
2020-11-22 16:08:08 +0100

    feat: add a driver for sg90 servo

---
+++ drivers/sg90/SG90.cpp
@@ -0,0 +1,25 @@
+#include "SG90.h"
+
+namespace exploringBB
+{
+    SG90::SG90(string pinName) : PWM(pinName)
+    {
+        setPeriod(DEFAULT_PERIOD);
+        setDutyCycle((unsigned int)MIN_DUTY_CYCLE);
+    }
+
+    void SG90::rotate(float angle)
+    {
+        unsigned int milliseconds = angleToMilliseconds(angle);
+        setDutyCycle(milliseconds * 1000000);
+    }
+
+    unsigned int angleToMilliseconds(float angle)
+    {
+        unsigned int percentage = (angle * 100) / 180;
+        float range = MAX_DUTY_CYCLE - MIN_DUTY_CYCLE;
+        return ((percentage * range) / 100) + MIN_DUTY_CYCLE;
+    }
+
+    SG90::~SG90() {}
+} // namespace exploringBB
---
+++ drivers/sg90/SG90.h
@@ -0,0 +1,26 @@
+#include "PWM.h"
+
+#define DEFAULT_PERIOD 20000000 // 20ms (50hz)
+#define MIN_DUTY_CYCLE 1000000  // 1ms
+#define MAX_DUTY_CYCLE 5000000  // 5ms
+
+namespace exploringBB
+{
+    /**
+     * @class HIH8120
+     * @brief Specific class for the HIH8120 Humidity & Temperature sensor that is a child of 
+     * the I2CDevice class. Protected inheritance means that the public I2CDevice methods are 
+     * not publicly accessible by an object of the HIH8120 class.
+     */
+    class SG90 : protected PWM
+    {
+
+    public:
+        SG90(string pinName);
+        virtual void rotate(float angle);
+        virtual ~SG90();
+
+    private:
+        unsigned int angleToMilliseconds(float angle);
+    };
+} /* namespace exploringBB */
