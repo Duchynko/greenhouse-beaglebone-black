19efd30a718d2e8342e555c0bac18a6f485990a5
Jakub Duchon <jakub.duchon@lego.com>
2020-11-23 02:23:56 +0100

    fix: add some fixes

--- drivers/pwm_light/main.cpp
+++ drivers/pwm_light/main.cpp
@@ -1,9 +1,9 @@
-#include "SG90.h"
+#include "PWM.h"
 
 int main(int argc, char const *argv[])
 {
     unsigned int value = atoi(argv[1]);
-    auto pwm = exploringBB::PWM("pwm-1:1");
-    pwm.setDutyCycle(value);
+    auto pwm = exploringBB::PWM("pwm-1:0");
+    pwm.setDutyCycle((float)value);
     return 0;
 }
--- drivers/sg90/SG90.cpp
+++ drivers/sg90/SG90.cpp
@@ -1,4 +1,5 @@
 #include "SG90.h"
+#include <iostream>
 
 namespace exploringBB
 {
@@ -11,14 +12,17 @@ namespace exploringBB
 
     void SG90::rotate(float angle)
     {
-        unsigned int dutyCycle = angleToDutyCycle(angle);
-        setDutyCycle(dutyCycle * 1000000);
+        double dutyCycle = angleToDutyCycle(angle);
+	unsigned int dcInNs = dutyCycle * 1000000 * 10;
+	std::cout << dcInNs << std::endl;
+	setDutyCycle(dcInNs);
     }
 
     double SG90::angleToDutyCycle(float angle)
     {
         unsigned int percentage = (angle * 100) / 180;
         unsigned int pulseWidth = (percentage * (MAX_DUTY_CYCLE - MIN_DUTY_CYCLE) / 100) + MIN_DUTY_CYCLE;
+	std::cout << pulseWidth << " " << (double)pulseWidth / DEFAULT_PERIOD << std::endl;
         return (double)pulseWidth / DEFAULT_PERIOD;
     }
 
